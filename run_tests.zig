4658c282da6c33fdf5101f8f2a9da9f8051f158b
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-07-14 00:18:31 +0530

    feat: Validate function parameter and return types for interface implementation

--- run_tests.zig
+++ run_tests.zig
@@ -4,11 +4,7 @@ const type_assertions = @import("src/root.zig");
 const TestCaseExpectation = type_assertions.compile_assertions.TestCaseExpectation;
 const run_test = type_assertions.compile_assertions.run_test;
 
-const test_cases = [_]TestCaseExpectation{.{
-    .file_path = "src/interface.1.test.zig",
-    .expected_exit_code = 1,
-    .in_stderr = "The return params for `some` do not match. Expected `void`. Found `u32`.",
-}};
+const test_cases = [_]TestCaseExpectation{};
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}).init;
--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,2 @@
 const std = @import("std");
-pub const interface = @import("interface.zig");
 pub const compile_assertions = @import("compile_assertions.zig");
